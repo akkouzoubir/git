1c286359aac2d8daa5b03a3261eb592366d9bf59
khaled <belala.khaled@hotmail.fr>
2016-09-29 11:21:07 +0200

    suppri

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 test
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta repudiandae, sunt eum modi quas totam sed, cumque architecto deserunt ut officiis temporibus voluptate, illum quia repellat quos consequatur numquam obcaecati.
+11111111111111111111111111
+j'ai changé
+orem ipsum dolor sit amet, consectetur adipisicing elit. Dicta repudiandae, sunt eum modi quas totam sed, cumque architecto deserunt ut officiis temporibus voluptate, illum quia repellat quos consequatur numquam obcaecati.
